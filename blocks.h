b5c5e9d0951a3471270b06779006c1ccc2c313e7
Ramon Bartl <rb@ridingbytes.com>
2022-05-12 21:45:47 +0200

    Added CPU status

--- blocks.h
+++ blocks.h
@@ -4,6 +4,7 @@ static const Block blocks[] = {
 	/* {"Mem:", "freecolor -o | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0}, */
 	/* {"", "date '+%b %d (%a) %I:%M%p'",					5,		0}, */
 
+	{"", "sb-cpu",        2,  18},
 	{"", "sb-network",    60, 17},
 	{"", "sb-battery",    60, 16},  // SIGUSR1-1 + 12 = 45
 	{"", "sb-brightness", 60, 15},  // SIGUSR1-1 + 15 = 44
