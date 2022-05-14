071db3e5b95f1341e11f6d270b003e3ed16e2afa
Ramon Bartl <rb@ridingbytes.com>
2022-05-14 11:00:32 +0200

    increased refresh timers

--- blocks.h
+++ blocks.h
@@ -4,12 +4,12 @@ static const Block blocks[] = {
 	/* {"Mem:", "freecolor -o | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0}, */
 	/* {"", "date '+%b %d (%a) %I:%M%p'",					5,		0}, */
 
-	{"", "sb-cpu",        2,  18},
+	{"", "sb-cpu",        5,  18},
 	{"", "sb-network",    60, 17},
 	{"", "sb-battery",    60, 16},  // SIGUSR1-1 + 12 = 45
 	{"", "sb-brightness", 60, 15},  // SIGUSR1-1 + 15 = 44
 	{"", "sb-volume",     0,  10},  // SIGUSR1-1 + 10 = 39
-	{"", "sb-clock",      1,  9},
+	{"", "sb-clock",      30,  9},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
